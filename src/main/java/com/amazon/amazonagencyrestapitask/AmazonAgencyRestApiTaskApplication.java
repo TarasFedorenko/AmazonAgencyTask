f9ec81238c79299abd78d2d72ed6b0d562afdc56
Fedorenko Taras <tarakhanio84@gmail.com>
2024-10-25 14:42:54 +0300

    Initial commit

---
+++ src/main/java/com/amazon/amazonagencyrestapitask/AmazonAgencyRestApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.amazonagencyrestapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonAgencyRestApiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonAgencyRestApiTaskApplication.class, args);
+    }
+
+}
